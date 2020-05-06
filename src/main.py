70eabeefc3dca7f4fa574e7d1555bfb95003bb69
Seungcheol Oh <seung9412@naver.com>
2020-05-06 17:53:01 +0900

    Complete

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from indeed import get_jobs as get_indeed_jobs
+from save import save_to_file
 # from so import get_jobs as get_so_jobs
 
 app=Flask("SuperScrapper")
 
+db={}
+
 @app.route("/")
 def home():
+    print(request)
     return render_template("home.html")
 
 @app.route("/report")
@@ -13,15 +17,36 @@ def report():
     word=request.args.get("word")
     if word:
         word=word.lower()
-        indeed_jobs=get_indeed_jobs(word)
-        # so_jobs= get_so_jobs(word)
-        if indeed_jobs==None:
-        # and so_jobs==None:
-            return render_template("none.html", word=word)
-        print(indeed_jobs)
+        dbWord=db.get(word)
+        if dbWord:
+            jobs=dbWord
+        else:
+            jobs=get_indeed_jobs(word)
+            if jobs==None:
+                return render_template("none.html", word=word)
+            db[word]=jobs  #indeed_jobs의 형태 [{'title':'모집공고','company':'주식회사'},{'title':'~~'}]    {'word': jobs의 형태 그대로}
     else:
         return redirect("/")
     # URL에서 report 뒤를 지우면 word는 None이 되어 error가 뜨니 이를 막자
-    return render_template("report.html", word=word)
+    return render_template("report.html", word=word, number=len(jobs), jobs=jobs)
+    # [] 안에 있는 {}의 숫자
 
-app.run(host="192.168.1.3")
+@app.route("/export")
+def export():
+    try:
+        word=request.args.get("word")
+        if not word:
+            raise Exception()
+        word=word.lower()
+        dbWord=db.get(word)
+        if not dbWord:
+            raise Exception()
+        save_to_file(dbWord)
+        # Web-scraping 폴더에 csv 파일이 저장된다.
+        return send_file("storage.csv")
+        # export를 누른 사람에게 다운로드된다.
+    except:
+        return redirect("/")
+
+
+app.run(host="192.168.1.6")
--- src/save.py
+++ src/save.py
@@ -7,3 +7,4 @@ def save_to_file(jobs):
     for job in jobs:
         writer.writerow(list(job.values()))
     return
+    # 여기서 return은 함수에서 빠져나오는 return인 것 같다.
